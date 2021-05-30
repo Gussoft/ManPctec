73899a1cf64affe9b0049a8a7655ef354f2576f7
Guss <analista2@bigprime.pe>
2021-05-30 09:48:15 -0500

    Capa Entidades y Store Procedure

--- ManPcTec/CapaDatos/D_Categoria.cs
+++ ManPcTec/CapaDatos/D_Categoria.cs
@@ -6,11 +6,77 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
+using CapaEntidad;
 
 namespace CapaDatos
 {
     public class D_Categoria
     {
         SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString);
+    
+        public List<E_Categoria> ListarCategoria(string buscar)
+        {
+           
+            SqlDataReader LeerFilas;
+            SqlCommand cmd = new SqlCommand("SP_BUSCARCATEGORIA", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conn.Open();
+
+            cmd.Parameters.AddWithValue("@BUSCAR", buscar);
+            LeerFilas = cmd.ExecuteReader();
+            List<E_Categoria> Listar = new List<E_Categoria>();
+            while (LeerFilas.Read())
+            {
+                Listar.Add(new E_Categoria
+                {
+                    IdCat = LeerFilas.GetInt32(0),
+                    CodigoCat = LeerFilas.GetString(1),
+                    NombreCat = LeerFilas.GetString(2),
+                    DescripcionCat = LeerFilas.GetString(3)
+                });
+            }
+            conn.Close();
+            LeerFilas.Close();
+
+            return Listar;
+        }
+
+        public void InsertarCategoriaa(E_Categoria categoria)
+        {
+            SqlCommand cmd = new SqlCommand("SP_INSERTARCATEGORIA", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conn.Open();
+
+            cmd.Parameters.AddWithValue("@NOMBRE", categoria.NombreCat);
+            cmd.Parameters.AddWithValue("@DESCRIPCION", categoria.DescripcionCat);
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public void EditarCategoria(E_Categoria categoria)
+        {
+            SqlCommand cmd = new SqlCommand("SP_EDITARCATEGORIA", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conn.Open();
+
+            cmd.Parameters.AddWithValue("@IDCATEGORIA", categoria.IdCat);
+            cmd.Parameters.AddWithValue("@NOMBRE", categoria.NombreCat);
+            cmd.Parameters.AddWithValue("@DESCRIPCION", categoria.DescripcionCat);
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public void EliminarCategoria(E_Categoria categoria)
+        {
+            SqlCommand cmd = new SqlCommand("SP_ELIMINARCATEGORIA", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conn.Open();
+
+            cmd.Parameters.AddWithValue("@IDCATEGORIA", categoria.IdCat);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
     }
 }
---
+++ ManPcTec/CapaEntidad/E_Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class E_Categoria
+    {
+        private int _idCat;
+        private string _codigoCat;
+        private string _nombreCat;
+        private string _descripcionCat;
+
+        public int IdCat { get => _idCat; set => _idCat = value; }
+        public string CodigoCat { get => _codigoCat; set => _codigoCat = value; }
+        public string NombreCat { get => _nombreCat; set => _nombreCat = value; }
+        public string DescripcionCat { get => _descripcionCat; set => _descripcionCat = value; }
+    }
+}
