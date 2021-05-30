0813579252023765731647a34104294f8a949a3e
Guss <analista2@bigprime.pe>
2021-05-30 09:13:21 -0500

    capa datos

---
+++ ManPcTec/CapaDatos/D_Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CapaDatos
+{
+    public class D_Categoria
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString);
+    }
+}
